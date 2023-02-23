b266cceac495fb51732f1610ad7d4134b0d7530b
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-24 00:33:36 +0530

    [Piyush] added to identify each address book with name and their type - family,friend and profession

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -40,3 +40,15 @@ SELECT COUNT(state) FROM AddressBook
 
 -- UC_8 - To retrieve entries sorted alphabetically contact by name for a city
 SELECT * from AddressBook WHERE city='Howrah' ORDER By firstname
+
+-- UC_9 - To identify each address book with name and type
+ALTER TABLE AddressBook add 
+book_name VARCHAR(20),
+contact_type VARCHAR(20)
+-- Update value for type=friend
+update AddressBook set book_name='FriendName',contact_type='Friends' WHERE firstname='Ravi'
+-- Update value for type=family
+update AddressBook set book_name='FamilyName',contact_type='Family' WHERE firstname='Ankita'
+-- Update value for type=profession
+update AddressBook set book_name='SoftwareDeveloper',contact_type='Profession' WHERE firstname='Piyush'
+
