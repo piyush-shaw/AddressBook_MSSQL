c0bda6d3251da0de4c23e0a515c99130f33afcac
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-23 19:30:08 +0530

    [Piyush] added to create a address book table

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -4,3 +4,14 @@ CREATE DATABASE AddressBookService
 -- To use created database
 USE AddressBookService
 
+-- UC_2 - To create table
+CREATE TABLE AddressBook(
+    firstname VARCHAR(200) not null,
+    lastname VARCHAR(200) not null,
+    address VARCHAR(250) not null,
+    city VARCHAR(50),
+    state VARCHAR(100),
+    zip int,
+    phonenumber bigint,
+    email VARCHAR(300) not null
+)
